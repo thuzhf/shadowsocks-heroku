e024f28111aa7f16aca5c349913fcc62b05ac75e
thuzhf <thuzhf@gmail.com>
2017-07-11 18:00:59 +0800

    test

--- setup_local_servers.py
+++ setup_local_servers.py
@@ -16,10 +16,15 @@ def gen_name_list(root_str, length, times):
     return results
 
 def setup_local(name_list, start_port):
-    subprocess.run('pkill -f "node local.js -s"')
-    subprocess.run('npm install', shell=True)
+    cli = 'pkill -f "node local.js -s"'
+    print('Run: {}'.format(cli))
+    subprocess.run(cli, shell=True)
+    cli = 'npm install'
+    print('Run: {}'.format(cli))
+    subprocess.run(cli, shell=True)
     for i, app_name in enumerate(name_list):
         cli = 'node local.js -s {0}.herokuapp.com -l {1} > /tmp/{0} &'.format(app_name, start_port + i)
+        print('Run: {}'.format(cli))
         subprocess.Popen(cli, shell=True)
 
 def get_IP(url, queue):
